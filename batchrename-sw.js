846c909115040ecd36bd934c01d74e4a9060c696
yumelyrics <282556325+yumelyrics@users.noreply.github.com>
2026-06-28 14:52:38 +0700

    Add files via upload

---
+++ batchrename-sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'batchrename-v1';
+const ASSETS = [
+  'batchrename.html',
+  'jszip.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(cache => cache.put(e.request, clone));
+      return res;
+    }))
+  );
+});
